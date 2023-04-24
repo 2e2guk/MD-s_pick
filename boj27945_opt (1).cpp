8226583a898ddd9f88f4b3189094bb1a9546acf2
이강욱 <90788409+2e2guk@users.noreply.github.com>
2023-04-24 19:31:40 +0900

    Update boj27945_opt (1).cpp

--- boj27945_opt (1).cpp
+++ boj27945_opt (1).cpp
@@ -1,6 +1,5 @@
-// boj 27945 슬슬 가지를 먹지 않으면 죽는다.
-// 실행 시간 252ms 메모리 17360KB
-// 최적화 내용 : 가중치에 대한 정보를 set 대신 vector에 저장, MEX찾는 과정을 최적화.
+// boj 27945 슬슬 가지를 먹기 않으면 죽는다
+// 실행 시간 280ms 메모리 17364KB
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -60,8 +59,9 @@ pair<int, vector<Edge>> kruskal_opt(int n, const vector<Edge>& edges) {
     UnionFind uf(n);
     vector<Edge> mst_edges;
     priority_queue<Edge> pq(edges.begin(), edges.end());
+    int edge_count = 0;
 
-    while (!pq.empty()) {
+    while (!pq.empty() && edge_count < n - 1) {
         Edge edge = pq.top();
         pq.pop();
 
@@ -69,10 +69,26 @@ pair<int, vector<Edge>> kruskal_opt(int n, const vector<Edge>& edges) {
             uf.unionSets(edge.u, edge.v);
             mst_cost += edge.weight;
             mst_edges.push_back(edge);
+            edge_count++;
         }
     }
 
     return make_pair(mst_cost, mst_edges);
+}
+
+// MEX란, 주어진 집합에 포함되지 않은 가장 작은 양의 정수이다.
+// 예를 들어 주어진 집합이 {0, 1, 2, 4, 5}라고 가정해보자.
+// 이 경우, 3은 집합에 포함되지 않은 가장 작은 양의 정수이므로 MEX는 3이다.
+int find_mex(const set<int>& weights) {
+    int mex = 1;
+    for (const auto& weight : weights) {
+        if (weight == mex) {
+            mex++;
+        } else {
+            break;
+        }
+    }
+    return mex;
 }
 
 int main() {
@@ -89,24 +105,12 @@ int main() {
 
     pair<int, vector<Edge>> MST = kruskal_opt(n, edges);
 
-    // MEX를 찾는 과정
-    bool has_one = false;
-    int mex = 1;
-
+    set<int> weights;
     for (const auto& edge : MST.second) {
-        if (edge.weight == 1) {
-            has_one = true;
-        } else if (has_one && edge.weight != mex) {
-            // 누락된 양의 정수인 MEX를 찾은 경우. 
-            break;
-        }
-        mex++;
+        weights.insert(edge.weight);
     }
 
-    if (!has_one) {
-        mex = 1;
-    }
-
+    int mex = find_mex(weights);
     cout << mex;
 
     return 0;
